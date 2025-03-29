25d954a54bdf541e18eeb00d90770b24c5736da0
muhammadibrahim <mik237@gmail.com>
2025-03-29 17:05:01 +0500

    added AppModule

--- app/src/main/java/me/ibrahim/bonialbrochures/presentation/MainViewModel.kt
+++ app/src/main/java/me/ibrahim/bonialbrochures/presentation/MainViewModel.kt
@@ -1,12 +1,17 @@
 package me.ibrahim.bonialbrochures.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import me.ibrahim.bonialbrochures.domain.repositories.BrochuresRepository
 import javax.inject.Inject
 
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    brochuresRepository: BrochuresRepository
+) : ViewModel() {
 
-
+    val brochures = brochuresRepository.getBrochures().cachedIn(viewModelScope)
 }
